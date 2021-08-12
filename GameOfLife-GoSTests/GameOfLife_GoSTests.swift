15dde10ec479344695ae545c9557b89aa37d50ed
Boris Rashkov <boris@diomerc.com>
2021-08-12 20:55:59 +0300

    Unit Tests Added

--- GameOfLife-GoSTests/GameOfLife_GoSTests.swift
+++ GameOfLife-GoSTests/GameOfLife_GoSTests.swift
@@ -10,6 +10,8 @@ import XCTest
 
 class GameOfLife_GoSTests: XCTestCase {
 
+    private let gameOfLife = Game(width: 3, height: 3)
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -17,17 +19,54 @@ class GameOfLife_GoSTests: XCTestCase {
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    /* TEST #1
+     * If a dead cell has 3 alive neighbours, it should become alive
+     */
+    func testBirthOfACell() {
+        let gameBoard = GameState(cells:[CellModel.makeLiveCell(), CellModel.makeDeadCell(), CellModel.makeDeadCell(),
+                                         CellModel.makeLiveCell(), CellModel.makeLiveCell(), CellModel.makeDeadCell(),
+                                         CellModel.makeDeadCell(), CellModel.makeDeadCell(), CellModel.makeDeadCell()
+            ])
+        gameOfLife.setInitialState(gameBoard)
+        XCTAssertTrue(gameOfLife.state(x: 1, y: 0))
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    /* TEST #2
+     * If a cell has 2 live neighbors it should survive
+     */
+    func testSurvivalOfCellWithTwoNeighbours() {
+        
+        let boardWithTwoLiveCells = GameState(cells: [CellModel.makeDeadCell(), CellModel.makeDeadCell(), CellModel.makeDeadCell(),
+                                                      CellModel.makeLiveCell(), CellModel.makeLiveCell(), CellModel.makeLiveCell(),
+                                                      CellModel.makeDeadCell(), CellModel.makeDeadCell(), CellModel.makeDeadCell()])
+        gameOfLife.setInitialState(boardWithTwoLiveCells)
+        XCTAssertTrue(gameOfLife.state(x: 1, y: 1))
     }
-
+    
+    /* TEST #3
+     * If a cell has 3 live neighbors it should survive
+     */
+    func testSurvivalOfCellWithThreeNeighbours() {
+        
+        let boardWithThreeLiveCells = GameState(cells: [CellModel.makeDeadCell(), CellModel.makeLiveCell(), CellModel.makeDeadCell(),
+                                                        CellModel.makeDeadCell(), CellModel.makeLiveCell(), CellModel.makeLiveCell(),
+                                                        CellModel.makeDeadCell(), CellModel.makeLiveCell(), CellModel.makeDeadCell()])
+        
+        gameOfLife.setInitialState(boardWithThreeLiveCells)
+        XCTAssertTrue(gameOfLife.state(x: 1, y: 1))
+    }
+    
+    /* TEST #4
+     * If a cell has NO alive neighbors it should die
+     */
+    func testIfACellDies() {
+        
+        let boardWithLonelyState = GameState(cells: [CellModel.makeDeadCell(), CellModel.makeDeadCell(), CellModel.makeDeadCell(),
+                                            CellModel.makeDeadCell(), CellModel.makeLiveCell(), CellModel.makeDeadCell(),
+                                            CellModel.makeDeadCell(), CellModel.makeDeadCell(), CellModel.makeDeadCell()])
+        
+        gameOfLife.setInitialState(boardWithLonelyState)
+        XCTAssertEqual(false, gameOfLife.state(x: 1, y: 1))
+        
+    }
 }
